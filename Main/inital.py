f3ce52eadf06a1a058944108b8c1f0ce98da03ed
stephen-becker <stephenbeckerjr@gmail.com>
2021-12-02 14:39:51 -0500

    2

--- Main/inital.py
+++ Main/inital.py
@@ -8,4 +8,13 @@ def GetTitleText(sectionNeeded):
     elif sectionNeeded == 2:
         return colored('Google Drive Connector:', 'red')
     elif sectionNeeded == 3:
-        return colored('System Status:', 'red')
+        return colored('Notebook Compatibility:', 'gold')
+
+def CheckNotebook(gpuRAM):
+
+    if gpuRAM <= 15000:
+        return colored('Limited RAM, consider using 1b_lyrics!', 'red')
+    elif gpuRAM > 15000:
+        return colored('With current RAM, most/all models should work fine.', 'green')
+    else:
+        return colored('Could not evaluate.', 'red')
