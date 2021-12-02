e9c2844e596619b7ead407c16f6d395bcfba9a69
stephen-becker <stephenbeckerjr@gmail.com>
2021-12-02 14:12:42 -0500

    2

--- Main/inital.py
+++ Main/inital.py
@@ -1,1 +1,12 @@
-!nvidia-smi -L
+import sys
+from termcolor import colored, cprint
+
+def GetTitleText(sectionNeeded):
+
+    match sectionNeeded:
+        case 1:
+            return colored('Assigned GPU:', 'red')
+        case 2:
+            return colored('Google Drive Connector:', 'red')
+        case 3:
+            return colored('System Status:', 'red')
